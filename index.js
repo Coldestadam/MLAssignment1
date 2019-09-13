a99514035d3e4471a6601ff04d06106ae8e52508
Adam VIllarreal <37571407+Coldestadam@users.noreply.github.com>
2019-09-13 02:01:53 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+const img_output =  document.getElementById('img-output');
+
+function loadFile(event){
+  img_output.src = URL.createObjectURL(event.target.files[0]);
+}
+
+async function classifyImg(){
+  console.log("Classify in progress...");
+  const result = await net.classify(img_output);
+  console.log("Classification completed");
+  document.getElementById('answer').innerText = `
+    Prediction: \n${result[0].className}\n
+    Probability: \n${result[0].probability}\n
+  `
+}
+
+async function loadApp(){
+  // load the model
+  console.log('Loading mobilenet...');
+  net = await mobilenet.load();
+  console.log('Sucessfully loaded model');
+  document.getElementById('classify').addEventListener("click", classifyImg);
+}
+
+loadApp();
